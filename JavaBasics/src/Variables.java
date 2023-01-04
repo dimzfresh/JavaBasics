2abbf79f6da8f3f350eafa256017f9fc85ccdb2c
Dimitrii Ziablikov <dimzfresh@gmail.com>
2023-01-04 14:16:27 +0300

    Basics: finish variables

---
+++ JavaBasics/src/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 123;
+        short myShort = 111;
+        long myLong = 123456;
+
+        double myDouble = 123.33;
+        float myFloat = 123.3f;
+
+        boolean myBool = (myInt == myShort);
+
+        char myChar = 'd';
+
+        byte myByte = 100;
+
+        System.out.println(myBool);
+    }
+}
