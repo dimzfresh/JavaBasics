ed59356e46ca97739ad02f305370781a449cad84
Dimitrii Ziablikov <dimzfresh@gmail.com>
2023-01-05 14:14:48 +0300

    Basics: finish strings

---
+++ JavaBasics/src/Strings.java
@@ -0,0 +1,9 @@
+public class Strings {
+    public static void main(String[] args) {
+        String firstName = "Dimitrii";
+        String spacer = " ";
+        String lastName = "Ziablikovskii";
+        String fullName = firstName + spacer + lastName;
+        System.out.println("My name is " + fullName);
+    }
+}
