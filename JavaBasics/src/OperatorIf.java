cbd466f394c43353213cadcdd0b9ea5a027c0a9d
Dimitrii Ziablikov <dimzfresh@gmail.com>
2023-01-05 20:17:28 +0300

    Basics: finish operator 'if'

---
+++ JavaBasics/src/OperatorIf.java
@@ -0,0 +1,10 @@
+public class OperatorIf {
+    public static void main(String[] args) {
+        int number = 7;
+        if (number == 7) {
+            System.out.println("Number == " + number);
+        } else {
+            System.out.println("Nothing happened");
+        }
+    }
+}
